2f87810298b5f2ca62bc6da08c4859db79a73049
nialloc9@gmail.com <nialloc9@gmail.com>
2016-08-21 19:43:11 +0900

    finished

---
+++ js/inputChecker.js
@@ -0,0 +1,72 @@
+//INPUT CHANGE
+function inputChecker(className, btnId, messageAreaId, messageError, messageSuccess){
+    var c = '.'+className;
+    var btn = '#'+ btnId;
+
+    //on key down
+    $(c).keydown(function(){
+        var infoArea = '#' + messageAreaId;
+
+
+
+        $(c).on('change', function () {
+            var reqlength = $(c).length;
+            console.log(reqlength);
+            var value = $(c).filter(function () {
+                return this.value != '';
+            });
+
+            if(value.length>=0 && (value.length !== reqlength)) {
+                pOrN = true;
+            } else {
+                pOrN = false;
+            }
+
+            //add styling
+            if(pOrN){
+                if($(infoArea).hasClass('success-message')){
+                    $(infoArea).removeClass('success-message');
+                }
+
+                if($(infoArea).hasClass('error-message')){
+
+                }else{
+                    $(infoArea).addClass('error-message');
+                }
+
+                //add error message
+                message = messageError;
+
+                //disable button
+                if($(btn).attr('disabled') == 'disabled'){
+
+                }else{
+                    $(btn).attr('disabled', 'disabled');
+                }
+            }else{
+                if($(infoArea).hasClass('error-message')){
+                    $(infoArea).removeClass('error-message');
+                }
+
+                if($(infoArea).hasClass('success-message')){
+
+                }else{
+                    $(infoArea).addClass('success-message');
+                }
+
+                //add success message
+                message = messageSuccess;
+
+                //enable button
+                if($(btn).attr('disabled') == 'disabled'){
+                    $(btn).removeAttr('disabled');
+                }else{
+
+                }
+            }
+
+            //render info
+            $(infoArea).text(message);
+        });
+    });
+}
